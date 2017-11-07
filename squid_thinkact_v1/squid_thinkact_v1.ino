03e56cbde4e02a1d71695a0e2dc4897f9e97d6ff
Paul <paul.nadan@students.olin.edu>
2017-11-07 15:45:56 -0500

    Edited the act code

--- squid_thinkact_v1/squid_thinkact_v1.ino
+++ squid_thinkact_v1/squid_thinkact_v1.ino
@@ -5,7 +5,7 @@
  * Mission: Drive straight to buoy, turn in circle, drive to next buoy, etc.,
  *          then back home
  * Team Squid: Aubrey, Diego, Gretchen, Jon, MJ, Paul  
- * 11/3/2017
+ * 11/7/2017
  * Version 1
  */
 
@@ -30,17 +30,26 @@
 // Constants
 enum {RIGHT=-1, NONE=0, LEFT=1, STRAIGHT=2}; // Directions
 enum {RED=3, YELLOW=4, WHITE=5, HOME=6, DANCE=7, LOOP=8}; // Targets
-const int MIN_DIST = 10; // Distance from target to start turning, in inches
+const int APPROACH_DIST = 10; // Distance from target to start turning, in inches
 const float K_P = 1.0; // Proportional constant for feedback control
-const int VELOCITY = 100; // Pump output for normal swimming
+const int FORWARD_VELOCITY = 150; // Pump output for normal swimming
+const int TURNING_VELOCITY = 150; // Pump output for turning
 const int MAX_MISSION_LENGTH = 10; // Maximum number of targets in a mission
 const int CAMERA_RATIO = 1; // Distance from buoy divided by pixel width of buoy
+const int SERVO_MAX_POSITION = 170; // Maximum angle that servos can output
+const int FIN_FORWARD_ANGLE = 10; // Left fin servo value for going forward (right is reversed)
+const int FIN_TURN_ANGLE = 170; // Left fin servo value for turning (right is reversed)
+const int TUBE_ZERO_ANGLE = 0; // Left tube servo default position for going forward (right is reversed)
+const int TURNING_ANGLE = 170; // Angle output for initiating a turn, cutoff for applying valves and fins
+const bool TURN_TUBES = true; // Whether to use tube servos for steering
+const bool TURN_VALVES = true; // Whether to use valves for steering
+const bool TURN_FINS = true; // Whether to use fins for steering
 
 // Pins
 const int FIN1 = 3; //right fin
 const int FIN2 = 9; //left fin
 const int TUBE1 = 10; //right tube pull
-const int TUBE2 = 11 //left tube pull
+const int TUBE2 = 11; //left tube pull
 const int VALVE2 = 12; //left valve through relay
 const int STOP = 4; //magnetic sensor pin to determin eStop
 
@@ -49,7 +58,7 @@ Pixy pixy; //creates PixyCam object to use
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //creates motor shield
 Adafruit_DCMotor *pump = AFMS.getMotor(1); //create bilge pump DC motor plugged into motor shield
 Adafruit_DCMotor *valve1 = AFMS.getMotor(2); //right valve
-Servo rightFin, leftFin;
+Servo rightFin, leftFin, leftTube, rightTube;
 EasyTransfer ETin, ETout; 
 SoftwareSerial XBee(2, 3); // RX, TX
 
@@ -59,14 +68,10 @@ int *mission[MAX_MISSION_LENGTH]; // Ordered array of targets, e.g. {RED, YELLOW
 int target = 0; // Current target index
 int distance = 0; // Distance from target in inches
 int angle = 0; // Angle towards target in degrees CCW
-long previousMillis = 0;
-//eStop true if eStop activated
-//flood true if hull flooding
-//temp true if electronics overheating
-boolean eStop, flood, temp = false;
+long previousMillis = 0; // Previous loop time in milliseconds
+boolean eStop, flood, temp = false; // eStop activated, hull flooding, electronics overheating
 
-
-//Serial send/recieve structures
+// Serial send/recieve structures
 struct RECEIVE_DATA_STRUCTURE{
   //put your variable definitions here for the data you want to receive
   //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
@@ -81,7 +86,7 @@ struct SEND_DATA_STRUCTURE{
   array blocks;
 };
 
-//give a name to the group of data
+// Give a name to the group of data
 RECEIVE_DATA_STRUCTURE rxdata;
 SEND_DATA_STRUCTURE txdata;
 
@@ -89,11 +94,11 @@ SEND_DATA_STRUCTURE txdata;
 void setup() {
   Serial.begin(9600);
   XBee.begin(9600);
-  //Serial transfer stuff
+  //Serial transfer initialization
   ETin.begin(details(rxdata), &Serial);
   ETout.begin(details(txdata), &Serial);
 
-  AFMS. begin();
+  AFMS.begin();
   
   pixy.init();
   
@@ -103,21 +108,19 @@ void setup() {
   leftTube.attach(TUBE2);
 
   pump -> setSpeed(150);
-  valve1 -> setSpeed(150); //right
+  valve1 -> setSpeed(255); //right
 
   pinMode(VALVE2, OUTPUT); //left
   pinMode(STOP, INPUT);
   
   digitalWrite(STOP, HIGH);
   
-
   attachInterrupt(digitalPinToInterrupt(STOP), eStop, LOW);
 
   systemCheck();
 
   //delay before start code
   wait(5000);
-
 }
 
 
@@ -240,7 +243,7 @@ void think() {
     direction = DANCE;
   } else if (distance<0) { // Target not visible
     direction = LEFT;
-  } else if (distance<MIN_DIST) { // Reached target
+  } else if (distance<APPROACH_DIST) { // Reached target
     target++;
     if(mission[target]==LOOP) { // Restart mission
       target=0;
@@ -254,45 +257,64 @@ void think() {
 
 // ACT AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 void act() {
-  // TODO
   switch (direction) {
     case STRAIGHT: // Swim straight using proportional feedback control
       int angleOut = int(K_P*angle);
-      int distOut = VELOCITY;
-      valve1 -> run(FORWARD);
-      digital.write(VALVE2, HIGH);
-      move(angleOut, distOut);
+      move(FORWARD_VELOCITY, int(K_P*angle));
       break;
     case LEFT: // Turn left
-      int angleOut = int(K_P*angle);
-      int distOut = VELOCITY;
-      valve1 -> run(FORWARD);
-      digital.write(VALVE2, LOW);
-      move(angleOut, distOut);
-      leftFin.write(-90);
+      move(TURNING_VELOCITY, TURNING_ANGLE);
       break;
     case RIGHT: // Turn right
-      int angleOut = int(K_P*angle);
-      int distOut = VELOCITY;
-      valve1 -> run(RELEASE);
-      digital.write(VALVE2, HIGH);
-      move(angleOut, distOut);
-      rightFin.write(90);
+      move(TURNING_VELOCITY, -TURNING_ANGLE);
       break;
     case DANCE: // Show off your moves
       break;
     default: // Stop
-      rightFin.write(0);
-      leftFin.write(0);
-      leftTube.write(0);
-      rightTube.write(0);
+      move(0, 0);
       break;
   }
 }
-  
-void move(ang){
-  leftTube.write(ang);
-  rightTube.write(ang);
+
+// Output motor values
+void move(int vel, int ang){
+  // Set pump output
+  pump -> setSpeed(vel);
+  if(vel>0) {
+    pump -> run(FORWARD);
+  } else {
+    pump -> run(RELEASE);
+  }
+  // Set tube angles
+  if(TURN_TUBES) {
+    int leftTubeAngle = min(max(TUBE_ZERO_ANGLE+ang, TUBE_ZERO_ANGLE), TUBE_ZERO_ANGLE+TURNING_ANGLE);
+    int rightTubeAngle = min(max(TUBE_ZERO_ANGLE-ang, TUBE_ZERO_ANGLE), TUBE_ZERO_ANGLE+TURNING_ANGLE);
+    leftTube.write(leftTubeAngle);
+    rightTube.write(SERVO_MAX_POSITION-rightTubeAngle);
+  } else {
+    leftTube.write(TUBE_ZERO_ANGLE);
+    rightTube.write(SERVO_MAX_POSITION-TUBE_ZERO_ANGLE);
+  }
+  // Set fin angles
+  if(TURN_FINS && ang>=TURNING_ANGLE) { // left
+    leftFin.write(FIN_TURN_ANGLE);
+    rightFin.write(FIN_FORWARD_ANGLE);
+  } else if(TURN_FINS && ang<=-TURNING_ANGLE) { // right
+    leftFin.write(FIN_FORWARD_ANGLE);
+    rightFin.write(FIN_TURN_ANGLE);
+  } else { // straight
+    leftFin.write(FIN_FORWARD_ANGLE);
+    rightFin.write(FIN_FORWARD_ANGLE);
+  }
+  // Set valve states
+  if(TURN_VALVES && ang>=TURNING_ANGLE) { // left
+    valve1 -> run(RELEASE);
+    digital.write(VALVE2, HIGH);
+  } else if(TURN_VALVES && ang<=-TURNING_ANGLE) { // right
+    valve1 -> run(FORWARD);
+    digital.write(VALVE2, LOW);
+  } else { // straight
+    valve1 -> run(FORWARD);
+    digital.write(VALVE2, HIGH);
+  }
 }
-
-
